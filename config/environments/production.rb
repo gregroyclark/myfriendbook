0e31c86ccaf7600b9c86751f6d801bccb4b4fb61
peacefrog <gregroyclark@gmail.com>
2020-12-14 02:56:44 -0600

    adds action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+# add heroku info before pushing to production!
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
