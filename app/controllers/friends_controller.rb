9220d5d6a1a6fb9cf33f5376724bd1a440545600
peacefrog <gregroyclark@gmail.com>
2020-12-14 10:46:00 -0600

    adds before_action to prevent most actions before authentication

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,6 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /friends
   # GET /friends.json
